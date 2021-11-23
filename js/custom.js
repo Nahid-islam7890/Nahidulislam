1efdcc05247d876211bbdf7cda636bf9cecaca33
Nahid islam <nahid789096@gmail.com>
2021-11-23 10:30:31 +0600

    file-add

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,22 @@
 $(document).ready(
     function(){
-        $(".preloder").delay(2000).fadeOut();  
+        $(".preloder").delay(2000).fadeOut();
+        $(window).scroll(
+            function(){
+                var scrolling = $(this).scrollTop();
+                if(scrolling > 150){
+                    $(".navbar").addClass("fixed-Navbar");
+        
+                }else{
+                    $('.navbar').removeClass("fixed-Navbar")
+                }
+            }
+        )
+        $(function() {
+            $('.selector').animatedHeadline({
+                animationType: 'clip'
+            });
+          })
     }
    
 );
